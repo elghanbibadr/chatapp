a20030e2b175b58f6f5182a2eaa678ca02c0b5e5
badr ghanbi <bghanbi50@gmail.com>
2023-05-12 11:12:53 +0100

    create the navbar and the the basic css for the wrapper element

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar from './componenet/Navbar';
+
+
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+};
+
 
 const App = () => {
   return (
-    <h1>App</h1>
+    <div className={style.appContainer}>
+    <section className={style.sectionContainer}>
+      {/* Navbar */}
+      <Navbar />
+    </section>
+  </div>
   )
 }
 
