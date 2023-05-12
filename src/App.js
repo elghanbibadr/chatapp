825992fe001da264ed3f3f40c266a4f1dec67f4f
badr ghanbi <bghanbi50@gmail.com>
2023-05-12 17:47:19 +0100

    pull out data from firebase

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ const style = {
 
 const App = () => {
   const [user]=useAuthState(auth)
-  console.log(user)
   return (
     <div className={style.appContainer}>
     <section className={style.sectionContainer}>
--- src/componenet/Chat.jsx
+++ src/componenet/Chat.jsx
@@ -1,16 +1,34 @@
 import React ,{useState,useEffect} from 'react'
 import Message from './Message';
-
+import { collection,getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 const style = {
     main: `flex flex-col p-[10px]`,
   };
 
 const Chat = () => {
-  return (
+    const [messages,setMessages]=useState([])
+
+  useEffect((() =>{
+      async function fetchData() {
+          const querySnapshot = await getDocs(collection(db,'messages'));
+          let messgae=[]
+          querySnapshot.forEach((doc) => {
+          messgae.push({...doc.data(),id:doc.id})
+          });
+          setMessages(messgae)
+        }
+        
+      return () =>  fetchData();
+  }),[])
+
+    return (
     <>
     <main className={style.main}>
-        <Message/>
-    {/* chat message componenet */}
+        {/* chat message componenet */}
+        {messages && messages.map((message) => {
+        return  <Message key={message.id} message={message}/>
+        })}
     </main>
     {/* send message componenet */}
     <span></span>
